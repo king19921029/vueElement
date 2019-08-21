5b06b76b18efedcb35a41addcd04e93083a90734
judoulx <18801287297@163.com>
2019-08-21 20:12:34 +0800

    后管

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
                 target: 'https://oasis.littleoasisgroup.com/',//后端接口地址
                 changeOrigin: true,//是否允许跨越
                 pathRewrite: {
-                    '^/api': '',//重写,
+                    '^/apis': '',//重写,
                 }
             }
         },
